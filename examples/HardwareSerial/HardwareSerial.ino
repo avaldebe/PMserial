3453d14fef17cd413c19b09b863ad749b1cf83c1
Alvaro Valdebenito <avaldebe@gmail.com>
2020-04-16 21:59:50 +0200

    T/RH/HCHO output on examples, close #10

--- examples/HardwareSerial/HardwareSerial.ino
+++ examples/HardwareSerial/HardwareSerial.ino
@@ -31,6 +31,14 @@ void loop()
     // print formatted results
     Serial.printf("PM1.0 %2d, PM2.5 %2d, PM10 %2d [ug/m3]\n",
                   pms.pm01, pms.pm25, pms.pm10);
+
+    if (pms.has_number_concentration())
+      Serial.printf("N0.3 %4d, N0.5 %3d, N1.0 %2d, N2.5 %2d, N5.0 %2d, N10 %2d [#/100cc]\n",
+                    pms.n0p3, pms.n0p5, pms.n1p0, pms.n2p5, pms.n5p0, pms.n10p0);
+
+    if (pms.has_temperature_humidity() || pms.has_formaldehyde())
+      Serial.printf("%5.1f °C, %5.1f %%rh, %5.2f mg/m3 HCHO\n",
+                    pms.temp, pms.rhum, pms.hcho);
 #else
     // print the results
     Serial.print(F("PM1.0 "));
@@ -42,6 +50,40 @@ void loop()
     Serial.print(F("PM10 "));
     Serial.print(pms.pm10);
     Serial.println(F(" [ug/m3]"));
+
+    if (pms.has_number_concentration())
+    {
+      Serial.print(F("N0.3 "));
+      Serial.print(pms.n0p3);
+      Serial.print(F(", "));
+      Serial.print(F("N0.5 "));
+      Serial.print(pms.n0p5);
+      Serial.print(F(", "));
+      Serial.print(F("N1.0 "));
+      Serial.print(pms.n1p0);
+      Serial.print(F(", "));
+      Serial.print(F("N2.5 "));
+      Serial.print(pms.n2p5);
+      Serial.print(F(", "));
+      Serial.print(F("N5.0 "));
+      Serial.print(pms.n5p0);
+      Serial.print(F(", "));
+      Serial.print(F("N10 "));
+      Serial.print(pms.n10p0);
+      Serial.println(F(" [#/100cc]"));
+    }
+
+    if (pms.has_temperature_humidity() || pms.has_formaldehyde())
+    {
+      Serial.print(pms.temp, 1);
+      Serial.print(F(" °C"));
+      Serial.print(F(", "));
+      Serial.print(pms.rhum, 1);
+      Serial.print(F(" %rh"));
+      Serial.print(F(", "));
+      Serial.print(pms.hcho, 2);
+      Serial.println(F(" mg/m3 HCHO"));
+    }
 #endif
   }
   else
--- examples/debug/main.cpp
+++ examples/debug/main.cpp
@@ -51,12 +51,24 @@ void setup()
 
 void printPMS()
 {
+#if defined(ESP8266) || defined(ESP8266)
   if (!pms.has_particulate_matter())
     return;
-#if defined(ESP8266) || defined(ESP8266)
   Serial.printf("PM1.0 %2d, PM2.5 %2d, PM10 %2d [ug/m3]\n",
                 pms.pm01, pms.pm25, pms.pm10);
+
+  if (!pms.has_number_concentration())
+    return;
+  Serial.printf("N0.3 %4d, N0.5 %3d, N1.0 %2d, N2.5 %2d, N5.0 %2d, N10 %2d [#/100cc]\n",
+                pms.n0p3, pms.n0p5, pms.n1p0, pms.n2p5, pms.n5p0, pms.n10p0);
+
+  if (!pms.has_temperature_humidity() && !pms.has_formaldehyde())
+    return;
+  Serial.printf("%5.1f °C, %5.1f %%rh, %5.2f mg/m3 HCHO\n",
+                pms.temp, pms.rhum, pms.hcho);
 #else
+  if (!pms.has_particulate_matter())
+    return;
   Serial.print(F("PM1.0 "));
   Serial.print(pms.pm01);
   Serial.print(F(", "));
@@ -66,14 +78,9 @@ void printPMS()
   Serial.print(F("PM10 "));
   Serial.print(pms.pm10);
   Serial.println(F(" [ug/m3]"));
-#endif
 
   if (!pms.has_number_concentration())
     return;
-#if defined(ESP8266) || defined(ESP8266)
-  Serial.printf("N0.3 %4d, N0.5 %3d, N1.0 %2d, N2.5 %2d, N5.0 %2d, N10 %2d [#/100cc]\n",
-                pms.n0p3, pms.n0p5, pms.n1p0, pms.n2p5, pms.n5p0, pms.n10p0);
-#else
   Serial.print(F("N0.3 "));
   Serial.print(pms.n0p3);
   Serial.print(F(", "));
@@ -92,6 +99,17 @@ void printPMS()
   Serial.print(F("N10 "));
   Serial.print(pms.n10p0);
   Serial.println(F(" [#/100cc]"));
+
+  if (!pms.has_temperature_humidity() && !pms.has_formaldehyde())
+    return;
+  Serial.print(pms.temp, 1);
+  Serial.print(F(" °C"));
+  Serial.print(F(", "));
+  Serial.print(pms.rhum, 1);
+  Serial.print(F(" %rh"));
+  Serial.print(F(", "));
+  Serial.print(pms.hcho, 2);
+  Serial.println(F(" mg/m3 HCHO"));
 #endif
 }
 
